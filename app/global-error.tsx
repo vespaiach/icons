b1f2dd056c8d9892619ac81cbb44fbc3c6625dca
Trinh Nguyen <nta.toan@gmail.com>
2025-12-30 07:58:51 -0500

    fixing build

---
+++ app/global-error.tsx
@@ -0,0 +1,22 @@
+'use client'; // Error boundaries must be Client Components
+
+export default function GlobalError({
+    error,
+    reset
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    console.error(error);
+    return (
+        // global-error must include html and body tags
+        <html lang="en">
+            <body>
+                <h2>Something went wrong!</h2>
+                <button type="button" onClick={() => reset()}>
+                    Try again
+                </button>
+            </body>
+        </html>
+    );
+}
