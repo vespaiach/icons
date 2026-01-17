053b64fe922ba8ea1c5e46b1d044758fc2c0057d
Trinh Nguyen <nta.toan@gmail.com>
2026-01-17 14:07:50 -0500

    short cache 60s

--- app/(external)/ficons/page.tsx
+++ app/(external)/ficons/page.tsx
@@ -8,6 +8,9 @@ import { PageContextProvider } from './_components/PageContext';
 import SearchModal from './_components/SearchModal';
 import { getRepositoriesAction } from './actions';
 
+// Enable stale-while-revalidate caching: revalidate every 60 seconds
+export const revalidate = 60;
+
 export default async function PageIcons() {
     const repositoriesVariants = await getRepositoriesAction();
 
