b29f17c016542f2508822048ca44a122877f3021
Trinh Nguyen <nta.toan@gmail.com>
2026-01-23 16:46:46 -0500

    support mobile

--- app/(external)/ficons/_components/Navbar/IconsSectionLinks.tsx
+++ app/(external)/ficons/_components/Navbar/IconsSectionLinks.tsx
@@ -1,21 +1,28 @@
 'use client';
 
+import { useClickAway } from '@uidotdev/usehooks';
 import { useRef } from 'react';
 import { repoToId } from '@/utils/common-helpers';
 
 export default function IconsSectionLinks({ repositories }: { repositories: Repository[] }) {
     const detailRef = useRef<HTMLDetailsElement>(null);
+    const ref = useClickAway<HTMLUListElement>(() => {
+        if (detailRef.current) {
+            detailRef.current.open = false;
+        }
+    });
 
     return (
         <details ref={detailRef} className="d-dropdown d-dropdown-end">
             <summary className="d-btn d-btn-sm d-btn-ghost">Collections</summary>
             <ul
+                ref={ref}
                 tabIndex={-1}
                 className="d-dropdown-content d-menu bg-base-100 rounded-box z-1 w-62 p-2 shadow-sm">
                 {repositories.map((repo) => (
                     <li key={repo.id}>
                         <a
-                            className="d-link capitalize"
+                            className="capitalize"
                             href={`#${repoToId(repo)}`}
                             onClick={(e) => {
                                 e.preventDefault();
